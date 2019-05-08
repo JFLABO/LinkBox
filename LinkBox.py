9e745721dee2218ef3fbab270edcda3c31f7da08
JFLABO[JP] <tech@pjfs.biz>
2019-05-09 08:03:35 +0900

    Add files via upload

---
+++ LinkBox.py
@@ -0,0 +1,53 @@
+﻿# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'LinkBox.ui',
+# licensing of 'LinkBox.ui' applies.
+#
+# Created: Thu May  9 03:58:33 2019
+#      by: pyside2-uic  running on PySide2 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(756, 487)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(400, 450, 341, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.tableView = QtWidgets.QTableView(Dialog)
+        self.tableView.setGeometry(QtCore.QRect(10, 40, 731, 111))
+        self.tableView.setObjectName("tableView")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(550, 10, 191, 16))
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 541, 31))
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setObjectName("label_2")
+        self.tableView_2 = QtWidgets.QTableView(Dialog)
+        self.tableView_2.setGeometry(QtCore.QRect(10, 180, 731, 151))
+        self.tableView_2.setObjectName("tableView_2")
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(500, 150, 161, 31))
+        self.textEdit.setObjectName("textEdit")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(660, 150, 75, 31))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtWidgets.QApplication.translate("Dialog", "Dialog", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("Dialog", "令和元年　5月9日(木) 12:00", None, -1))
+        self.label_2.setText(QtWidgets.QApplication.translate("Dialog", "TeamMessage", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("Dialog", "検索", None, -1))
+
