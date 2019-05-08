b5a4559c62a3b62ba79a5a30fb8fb1bba94e5513
JFLABO[JP] <tech@pjfs.biz>
2019-05-09 08:03:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5 import QtWidgets
+from LinkBox import Ui_Dialog
+
+class Test(QtWidgets.QDialog):
+  def __init__(self,parent=None):
+    super(Test, self).__init__(parent)
+    self.ui = Ui_Dialog()
+    self.ui.setupUi(self)
+    #def slot1(self): #イベント処理の関数
+    #self.ui.label.setText('Hello World!')
+
+if __name__ == '__main__':
+  app = QtWidgets.QApplication(sys.argv)
+  window = Test()
+  window.show()
+  sys.exit(app.exec_())
+
 #magic
