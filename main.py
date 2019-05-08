7b37f0a2c007e834b5fc70e0741c391c83cfd0b0
JFLABO[JP] <tech@pjfs.biz>
2019-05-09 08:07:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 import sys
 from PyQt5 import QtWidgets
 from LinkBox import Ui_Dialog
-
+#実装開始　開発中
 class Test(QtWidgets.QDialog):
   def __init__(self,parent=None):
     super(Test, self).__init__(parent)
     self.ui = Ui_Dialog()
-    self.ui.setupUi(self)
+    #self.ui.setupUi(self)
     #def slot1(self): #イベント処理の関数
     #self.ui.label.setText('Hello World!')
 
