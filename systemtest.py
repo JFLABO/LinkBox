cf6280ee56f13794ea5a79880eacf4a30bbc2352
JFLABO[JP] <tech@pjfs.biz>
2019-05-11 02:36:28 +0900

    Update systemtest.py

--- systemtest.py
+++ systemtest.py
@@ -2,14 +2,19 @@
 import os
 import wx
 import psutil
+import platform
+
 # gives a single float value
 a=psutil.cpu_percent()
+
 # gives an object with many fields
 b=psutil.virtual_memory()
+
 # you can convert that object to a dictionary 
 c=dict(psutil.virtual_memory()._asdict())
 
+d=platform.processor()
 app = wx.App()
 
 # メッセージボックスを表示
-wx.MessageBox(u'TEST'+ str(a) + str(b) + str(c), u'タイトル')
+wx.MessageBox(u'SYSTEMTEST::'+d+ str(a) + str(b) + str(c), u'タイトル')
