0a0676bbc77b02fd9235a845341ce778003d5425
JFLABO[JP] <tech@pjfs.biz>
2019-05-12 19:35:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 # -*- coding: utf-8 -*-
-import sys
-from PyQt5 import QtWidgets
-from LinkBox import Ui_Dialog
-#実装開始　開発中
-class Test(QtWidgets.QDialog):
-  def __init__(self,parent=None):
-    super(Test, self).__init__(parent)
-    self.ui = Ui_Dialog()
-    #self.ui.setupUi(self)
-    #def slot1(self): #イベント処理の関数
-    #self.ui.label.setText('Hello World!')
+import wx
+import wx.grid
+import glob
+
+class GridFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent)
+
+        # Create a wxGrid object
+        grid = wx.grid.Grid(self, -1)
+
+        # Then we call CreateGrid to set the dimensions of the grid
+        # (100 rows and 10 columns in this example)
+        grid.CreateGrid(50, 1)
+
+        # We can set the sizes of individual rows and columns
+        # in pixels
+        grid.SetRowSize(0, 60)
+        grid.SetColSize(0, 120)
+
+        # And set grid cell contents as strings
+        #grid.SetCellValue(0, 0, 'wxGrid is good')
+        i=0
+        for name in glob.iglob('../**/*.url', recursive=True):
+        #print(name)
+            grid.SetCellValue(i, 0, name)
+            i=i+1
+      self.Show()
 
 if __name__ == '__main__':
-  app = QtWidgets.QApplication(sys.argv)
-  window = Test()
-  window.show()
-  sys.exit(app.exec_())
+
+    app = wx.App(0)
+    frame = GridFrame(None)
+    app.MainLoop()
 
 #magic
