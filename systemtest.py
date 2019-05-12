83e85dfafaf52fadb21604c69a9447071388ad5c
JFLABO[JP] <tech@pjfs.biz>
2019-05-12 17:07:52 +0900

    Update systemtest.py

--- systemtest.py
+++ systemtest.py
@@ -3,6 +3,11 @@ import os
 import wx
 import psutil
 import platform
+import subprocess  
+import multiprocessing
+
+info=subprocess.check_output(["wmic","cpu","get", "name"])  
+#print info.split('@')[1].split(' ')[1]
 
 # gives a single float value
 a=psutil.cpu_percent()
@@ -16,6 +21,6 @@ c=dict(psutil.virtual_memory()._asdict())
 d=platform.processor()
 app = wx.App()
 
+info2=multiprocessing.cpu_count()
 # メッセージボックスを表示
-wx.MessageBox(u'SYSTEMTEST::'+d+ str(a) + str(b) + str(c), u'この画面が出た場合はOKです。')
-# 出ない場合は環境にプログラムが足りない可能性があります。
+wx.MessageBox(u'SYSTEMTEST::'+d+ str(a) + str(b) + str(c)+str(info)+str(info2)+"Cores", u'タイトル')
