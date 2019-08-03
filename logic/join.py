80abe415bcee1ed1b7160bebefbb00b7de5c6618
JFLABO <vortex003@msn.com>
2019-08-03 17:30:30 +0900

    export

---
+++ logic/join.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from pandas import Series, DataFrame
+
+# データ1の準備
+LOG = 'data01.json'
+attri_data1 = open(LOG, 'r')
+data_frame1 = DataFrame(attri_data1)
+
+# データ2の準備
+LOG = 'data02.json'
+attri_data2 = open(LOG, 'r')
+data_frame2 = DataFrame(attri_data2)
+
+#print(pd.merge(data_frame1, data_frame2, how="inner" ,on="ID"))
+df=pd.merge(data_frame1, data_frame2, how="left" ,on="ID")
+#print(df.to_html())
+print df
